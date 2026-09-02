886d247d8e661bfc78235695317dd693f0a8a603
Luthfi <luthfi.ahmad51@ui.ac.id>
2026-09-02 14:31:07 +0700

    Added HTML content and CSS

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'myportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Email
