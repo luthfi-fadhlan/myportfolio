e4319865eb7547afb4029fa5a76959ef901dfc01
Luthfi <luthfi.ahmad51@ui.ac.id>
2026-09-14 23:29:04 +0700

    feat(section): add education section

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.utils import timezone
 
 class Experience(models.Model):
     EXPERIENCE_CHOICES = [
@@ -20,6 +21,36 @@ class Experience(models.Model):
     ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
+    
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
+
+class Education(models.Model):
+    EDUCATION_CHOICES = [
+        ('high-school', 'High School'),
+        ('bachelor', 'Bachelor'),
+        ('master', 'Master'),
+        ('phd', 'PhD'),
+        ('other', 'Other'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.CharField(max_length=255)
+    degree = models.CharField(max_length=20, choices=EDUCATION_CHOICES, default='bachelor')
+    field_of_study = models.CharField(max_length=255)
+    # Default ke waktu saat objek dibuat, dapat diubah
+    started_at = models.DateTimeField(default=timezone.now)
+    ended_at = models.DateTimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.degree} in {self.field_of_study} at {self.institution}"
+
+    def get_year_started(self):
+        return self.started_at.year if self.started_at else None
+
+    def get_year_ended(self):
+        return self.ended_at.year if self.ended_at else None
     
     @property
     def is_ongoing(self):
